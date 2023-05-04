24e5a4f353d0bb69a616d53fe840f4205912adbc
Brandon Macer <bobbysox322@gmail.com>
2023-05-03 21:35:55 -0400

    tidying

--- src/pages/animal.tsx
+++ src/pages/animal.tsx
@@ -7,11 +7,11 @@ import {
   Text,
   VStack,
 } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import axios from "axios";
 import { MongoChatMemory } from "./api/MongoChatMemory";
 import { MongoDocument } from "./api/mongo-document.type";
-import { Spacer } from "@chakra-ui/react";
+import { Spacer, HStack } from "@chakra-ui/react";
 
 const generateRandomId = (): string => {
   const letters = "abcdefghijklmnopqrstuvwxyz";
@@ -42,9 +42,23 @@ export default function Home() {
   const [isNew, setIsNew] = useState(false);
   const [loading, setLoading] = useState(true);
   const [aiStatement, setAiStatement] = useState("");
-  const [fullHistory, setFullHistory] = useState<MongoDocument[]>([]);
   const [userid, setUserid] = useState("");
+  const [fullHistory, setFullHistory] = useState<MongoDocument[]>([
+    {
+      userid,
+      timestamp: new Date().getDate(),
+      message: "I'm an animal.  Can you guess what I am?  Or just chat. :-)",
+      messageType: "ai",
+    },
+  ]);
   const [guessButtonDisabled, setGuessButtonDisabled] = useState(false);
+
+  const scrollContainerRef = useRef<HTMLDivElement>(null);
+  const bottomRef = useRef<HTMLDivElement>(null);
+
+  const scrollToBottom = () => {
+    bottomRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
 
   const handleNewGame = (userid: string) => {
     try {
@@ -62,13 +76,23 @@ export default function Home() {
 
   const handleGuess = (userid: string) => {
     console.log("handling guess...");
-    setTimeout(() => {}, 2000);
+    // setTimeout(() => {}, 2000);
     let data = {
       user: "John",
       message: guess,
       userid,
     };
     try {
+      setGuess("");
+      setFullHistory([
+        ...fullHistory,
+        {
+          userid,
+          timestamp: new Date().getDate(),
+          message: guess,
+          messageType: "human",
+        },
+      ]);
       axios.post("/api/animal", data).then((response) => {
         console.log(response.data);
         setAiStatement(response.data.r);
@@ -81,6 +105,7 @@ export default function Home() {
 
   useEffect(() => {
     setGuessButtonDisabled(false);
+    scrollToBottom();
   }, [fullHistory]);
 
   useEffect(() => {
@@ -89,84 +114,98 @@ export default function Home() {
   }, []);
 
   return (
-    <VStack spacing={4} align="center">
-      <Text fontSize="4xl">Guess the animal.  Powered by ChatGPT.</Text>
-      {loading ? (
-        <Box h="24" display="flex" justifyContent="center" alignItems="center">
-          <Spinner />
-        </Box>
-      ) : (
-        <>
-          <Text fontSize="lg" fontWeight="bold">
-            {name}
-          </Text>
+    <Box w="100vw" h="100vh" overscrollBehavior="none">
+      <VStack spacing={4} align="center">
+        <Text
+          textAlign="center"
+          w="100vw"
+          bgColor="#E6ADAD"
+          h="8vh"
+          p="20px"
+          fontSize="4xl"
+          fontFamily="Arial"
+          color="blue.500"
+        >
+          Animal Guessing Game
+        </Text>
 
-          <Box h="80vh" overflowY="scroll">
-            {fullHistory.map((item, index) => (
-              <>
-                {/* <Text key={index}>{JSON.stringify(item)}</Text> */}
-                <Flex w="100vw">
-                  {item.messageType === "ai" && <Spacer />}
-                  <Text
-                    border="2px solid blue"
-                    w="50%"
-                    p="10px"
-                    bgColor="lightblue"
-                    borderRadius="10px"
-                    margin="20px"
-                  >
-                    {item.message}
-                  </Text>
-                  {item.messageType === "human" && <Spacer />}
-                </Flex>
-              </>
-            ))}
-          </Box>
-          <Box>
-            <Flex>
-              <Input
-                onKeyUp={(e) => {
-                  if (e.key === "Enter") {
-                    if (guessButtonDisabled) return;
-                    setGuessButtonDisabled(true);
-                    handleGuess(userid);
-                  }
-                }}
-                w="600px"
-                type="text"
-                onChange={(e) => setGuess(e.target.value)}
-              />
-              {guessButtonDisabled ? (
-                <Spinner />
-              ) : (
-                <>
-                  <Button
-                    colorScheme="blue"
-                    onClick={() => {
-                      if (guessButtonDisabled) return;
-                      setGuessButtonDisabled(true);
-                      handleGuess(userid);
-                    }}
-                    disabled={guessButtonDisabled}
-                    ml="30px"
-                    mr="30px"
-                    w="200px"
-                  >
-                    Guess
-                  </Button>
-                  <Button
-                    colorScheme="blue"
-                    onClick={() => handleNewGame(userid)}
-                    w="200px"
-                  >
-                    New Game
-                  </Button>
-                </>
-              )}
+        <Box h="80vh" overflowY="scroll" ref={scrollContainerRef}>
+          {fullHistory.map((item, index) => (
+            <Flex
+              key={index}
+              w="100%"
+              justify={item.messageType === "ai" ? "flex-end" : "flex-start"}
+            >
+              {item.messageType === "ai" && <Spacer />}
+              <Box
+                w="50%"
+                p="10px"
+                bgColor={item.messageType === "ai" ? "lightblue" : "lightgray"}
+                borderRadius="10px"
+                margin="20px"
+              >
+                {item.message}
+              </Box>
+              {item.messageType === "human" && <Spacer />}
             </Flex>
-          </Box>
-        </>
-      )}
-    </VStack>
+          ))}
+          <div ref={bottomRef}></div>
+        </Box>
+
+        <Box h="10vh">
+          <HStack>
+            <Input
+              onKeyUp={(e) => {
+                if (e.key === "Enter") {
+                  if (guessButtonDisabled) return;
+                  setGuessButtonDisabled(true);
+                  handleGuess(userid);
+                }
+              }}
+              value={guess}
+              w="50vw"
+              type="text"
+              onChange={(e) => setGuess(e.target.value)}
+              placeholder="Enter your guess"
+              bg="white"
+              borderRadius="md"
+              border="1px solid gray"
+              p="2"
+            />
+
+            <Button
+              colorScheme="blue"
+              onClick={() => {
+                if (guessButtonDisabled) return;
+                setGuessButtonDisabled(true);
+                handleGuess(userid);
+              }}
+              disabled={guessButtonDisabled}
+              ml="30px"
+              mr="30px"
+              w="10vw"
+              minW="100px"
+            >
+              {guessButtonDisabled ? (
+                <Spinner mr="10px" size="sm" />
+              ) : (
+                <Text>Guess</Text>
+              )}
+            </Button>
+
+            <Button
+              colorScheme="blue"
+              onClick={() => handleNewGame(userid)}
+              w="10vw"
+              minW="70px"
+            >
+              Reset
+            </Button>
+          </HStack>
+        </Box>
+
+        <Flex h="2vh" />
+      </VStack>
+    </Box>
   );
 }
